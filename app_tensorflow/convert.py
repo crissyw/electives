b4ae77d630e4f45361d48534731ac966ddab1c42
Shani Hadiyanto <shanipribadi@gmail.com>
2020-08-07 08:27:02 +0700

    change convert.py to have a main function instead
    Change-Id: I86cbb0d6ac3ab2a46e82861911f8b082eed268f3

--- app_tensorflow/convert.py
+++ app_tensorflow/convert.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 
-model = tf.keras.models.load_model('keras_model.h5', compile=False)
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-tflite_model = converter.convert()
-with tf.io.gfile.GFile('model.tflite', 'wb') as f:
-  f.write(tflite_model)
+def main():
+    model = tf.keras.models.load_model('keras_model.h5', compile=False)
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = converter.convert()
+    with tf.io.gfile.GFile('model.tflite', 'wb') as f:
+      f.write(tflite_model)
+
+if __name__ == '__main__':
+    main()
